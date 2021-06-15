907b1a8e1c47cca2915b1307e8aa9d34729b85eb
TCP Student 15 <student15@techbridge.org>
2021-06-14 22:58:28 -0400

    Jquery-Bootstrap-Submitted-Assignment

--- script.js
+++ script.js
@@ -9,15 +9,55 @@ $( document ).ready(function() {
 
     $( ".changebtn2" ).click(function() {
         console.log("change btn 2 clicked");
-        $(".box2").css({"background":"red","border":"2px solid blue"});
+        $(".box2").addclass("newbox2");
     });
 
     $( ".changebtn3" ).click(function() {
         console.log("change btn 3 clicked");
+        $(".box3").addClass("newbox3");
     });
 
     $( ".changebtn4" ).click(function() {
         console.log("change all btn clicked");
+        $(".changeAllGrpMmbr").addClass("allBoxesChange");
     });
 
+    $( ".hidebtn1" ).click(function() {
+        $(".box1").addClass("hiddenBox");
+    });
+
+    $( ".hidebtn2" ).click(function() {
+        $(".box2").addClass("hiddenBox");
+    });
+
+    $( ".hidebtn3" ).click(function() {
+        $(".box3").addClass("hiddenBox");
+    });
+
+    $( ".hidebtn4" ).click(function() {
+        $(".changeAllGrpMmbr").addClass("allBoxesHide");
+    });
+
+    $( ".showall" ).click(function() {
+            $(".showAllGrpMmber").removeClass("allBoxesHide");
+            $(".showAllGrpMmber").removeClass("hiddenBox");
+    });
+
+    $( ".resetcolors" ).click(function() {
+        console.log("change all btn clicked");
+        $(".changeAllGrpMmbr").removeClass("allBoxesChange");
+
+        $(".box1").removeClass("newbox1");
+        $(".box2").removeClass("newbox2");
+        $(".box3").removeClass("newbox3");
+    });
+       
+
 });
+
+    
+
+    
+
+
+
